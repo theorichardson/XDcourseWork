992bf526afc1e0ca8d88f19b106ff26f1186aa46
theorichardson <74269570+theorichardson@users.noreply.github.com>
2020-12-10 12:41:12 -0500

    Add files via upload

---
+++ overcomplicatingThings2.js
@@ -0,0 +1,148 @@
+
+$("h1").hover(function(){
+
+	$("h1").css({"color":"blue"})
+})
+
+
+$(".w").hover(function(){
+
+	$(this).css({"color":"blue"})
+})
+
+
+$(".w").hover(function(){
+
+	$(this).addClass("a")
+})
+
+
+
+
+
+$(document).tooltip();
+
+
+
+					$(".title").click(function(){
+
+					$(document).scrollTo(".line1",550)
+					$(document).scrollTo(".top",450)
+
+					})
+
+
+
+				$(".number1").click(function(){
+
+				$(document).scrollTo(".aboveWalk",1300)
+
+				})
+
+
+
+
+						$(".number2").click(function(){
+
+						/*$(this).css({"color":"blue"})*/
+						$(document).scrollTo(".aboveDog",1800)
+						/*$(this).addClass("a")*/
+
+						})
+
+								$(".number3").click(function(){
+
+								/*$(".number2").css({"color":"blue"})*/
+								$(document).scrollTo(".aboveFountain",2300)
+
+								})
+
+										$(".up1").click(function(){
+
+										/*$(".number2").css({"color":"blue"})*/
+										$(document).scrollTo(".top",1000)
+
+										})
+
+
+
+
+/*
+var singleHover = 0;
+$(".number1").mouseover(function(){
+
+	singleHover++;
+
+	if (singleHover == 1) {
+
+		$(document).scrollTo(".dog",2000)
+
+	}
+
+})
+*/
+
+
+
+
+
+
+
+
+
+/*
+
+$("p").hover(function(){
+
+	$("p").css({"color":"orange"})
+})
+
+			$("h1").click(function(){
+
+			$("h1").css({"color":"yellow"})
+			})
+
+
+			$("h1").hover(function(){
+
+				$("h1").css({"color":"black"})
+			})
+
+										$("h3").click(function(){
+
+										$("h3").css({"color":"yellow"})
+										})
+
+
+										$("h3").hover(function(){
+
+										$("h3").css({"color":"black"})
+										})
+
+														$("h4").click(function(){
+
+														$("h4").css({"color":"yellow"})
+														})
+
+
+														$("h4").hover(function(){
+
+															$("h4").css({"color":"black"})
+														})
+
+
+
+						$("a").click(function(){
+
+						$("a").css({"color":"yellow"})
+						})
+
+
+						$("a").hover(function(){
+
+						$("a").css({"color":"orange"})
+						})
+
+						*/
+
+
